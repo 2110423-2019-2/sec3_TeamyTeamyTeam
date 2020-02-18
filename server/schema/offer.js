0a2c7f8eb90609c20a894a1991b0159d5cbfb361
nictrak <narin14175@gmail.com>
2020-02-18 22:59:24 +0700

    add offer database

---
+++ server/schema/offer.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var offerSchema = new Schema({
+    title: {type: String, required: true},
+    employerName: {type: String, required: true},
+    style: {type: String, required: true},
+    date: {type: String, required: true},
+    time: {type: String, required: true},
+    location: {type: String, required: true}
+})
+
+var Offer = mongoose.model('offer',offerSchema);
+
+module.exports = Offer;
