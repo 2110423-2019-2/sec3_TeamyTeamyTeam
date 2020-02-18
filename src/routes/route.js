74c44cc4588084311e17405b8d224289c83dc856
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-02-18 14:44:43 +0700

    Resolve Portfolio Component's props

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,25 +8,30 @@ import Search from "../components/search/search";
 import Portfolio from "../components/portfolio/portfolio";
 import JobOffer from "../components/JobOffer/offer";
 
-export default class Routing extends React.Component{
-  constructor(props){
+export default class Routing extends React.Component {
+  constructor(props) {
     super(props);
   }
-  
+
   appState = this.props.appState;
 
   login = (username, uid) => {
-    return this.props.login(username, uid)
-  }
-  render(){
+    return this.props.login(username, uid);
+  };
+  render() {
     return (
       <Switch>
-        <Route exact path="/home" component={() => <Home/>} />
-        <Route path="/login" render={() => <Login {...this.props}/>} />
-        <Route exact path="/signup" component={() => <SignUp/>} />
-        <Route exact path="/search" component={() => <Search/>} />
-        <Route exact path="/portfolio/:name" component={() => <Portfolio/>} />
-        <Route exact path="/offer" component={() => <JobOffer/>} />
+        <Route exact path="/home" component={() => <Home />} />
+        <Route path="/login" render={() => <Login {...this.props} />} />
+        <Route exact path="/signup" component={() => <SignUp />} />
+        <Route exact path="/search" component={() => <Search />} />
+        <Route
+          exact
+          path="/portfolio/:name"
+          render={props => <Portfolio {...props} />}
+        />{" "}
+        />
+        <Route exact path="/offer" component={() => <JobOffer />} />
       </Switch>
     );
   }
