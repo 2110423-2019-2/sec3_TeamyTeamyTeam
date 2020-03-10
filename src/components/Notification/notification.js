0a9d1a5508fa768fbd7aec388ab48b8302942495
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-03-10 15:41:59 +0700

    Add notification constraints

--- src/components/Notification/notification.js
+++ src/components/Notification/notification.js
@@ -3,7 +3,28 @@ import "../../stylesheets/notification.css";
 import NotificationBox from "./notificationBox";
 
 class Notification extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      UID: 0,
+      numberOfUnreadNotification: 0,
+      toTalnotification: 0,
+      notifications: []
+    };
+  }
+
+  componentDidMount() {
+    //Retrieve notification's object
+    let toTalnotification = 15; //จำนวนnotiทั้งหมดของusers
+    let numberOfUnreadNotification = 15; //จำนวนnotiที่ยังไม่ได้อ่าน
+    this.setState({ numberOfUnreadNotification });
+    //จะกำหนดให้แสดงผล10อันล่าสุดเท่านั้น แต่ตัวnotiที่ยังไม่อ่านจะแสดงตามจริง
+    let notifications = [];
+    for (let i = 0; i < Math.min(toTalnotification, 10); i += 1) {
+      notifications.push("");
+    }
+    this.setState({ notifications });
+  }
 
   render() {
     return (
@@ -16,15 +37,21 @@ class Notification extends Component {
           data-toggle="dropdown"
           aria-haspopup="true"
           aria-expanded="false"
-          data-badge="10"
         >
           <ion-icon name="notifications-outline"></ion-icon>
+          {this.state.numberOfUnreadNotification > 0 ? (
+            <span data-badge={this.state.numberOfUnreadNotification}></span>
+          ) : (
+            ""
+          )}
         </a>
         <ul className="dropdown-menu">
           <li className="head text-light bg-dark">
             <div className="row">
               <div className="col-lg-12 col-sm-12 col-12">
-                <span>Notifications (3)</span>
+                <span>
+                  Notifications ({this.state.numberOfUnreadNotification})
+                </span>
                 {/* <a href="" className="float-right text-light">
                   Mark all as read
                 </a> */}
@@ -32,12 +59,18 @@ class Notification extends Component {
             </div>
           </li>
           <div className="overflow-auto" style={{ maxHeight: "400px" }}>
-            <NotificationBox />
-            <NotificationBox />
-            <NotificationBox />
-            <NotificationBox />
-            <NotificationBox />
-            <NotificationBox />
+            {this.state.notifications.length > 0 ? (
+              this.state.notifications.map(() => <NotificationBox />)
+            ) : (
+              <div className="text-center m-3">
+                <span>
+                  <ion-icon
+                    name="notifications-off-outline"
+                    style={{ color: "#c9c4c2", fontSize: "36px" }}
+                  ></ion-icon>
+                </span>
+              </div>
+            )}
           </div>
 
           <li className="footer bg-dark text-center">
--- src/components/Notification/notificationBox.js
+++ src/components/Notification/notificationBox.js
@@ -1,23 +1,45 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class NotificationBox extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.handleRead = this.handleRead.bind(this);
+    this.state = {
+      type: "",
+      sender: "Alexander Wang", //userID
+      message: "Your job is accepted",
+      date: "Dec 12, 2019",
+      isRead: false
+    };
+  }
+
+  handleRead() {
+    this.setState({ isRead: true });
+  }
+
   render() {
+    const { sender, message, date, isRead } = this.state;
     return (
-      <li className="notification-box">
-        <div className="row">
-          <div className="col-lg-3 col-sm-3 col-3 text-center my-auto">
-            <img
-              src="https://randomuser.me/api/portraits/women/11.jpg"
-              className="w-50 rounded-circle "
-            />
+      <li
+        className={isRead ? "notification-box" : "notification-box bg-gray"}
+        onClick={this.handleRead}
+      >
+        <Link to="#" className="nav-link">
+          <div className="row">
+            <div className="col-lg-3 col-sm-3 col-3 text-center my-auto">
+              <img
+                src="https://randomuser.me/api/portraits/women/11.jpg"
+                className="w-50 rounded-circle "
+              />
+            </div>
+            <div className="col-lg-8 col-sm-8 col-8">
+              <strong className="text-primary">{sender}</strong>
+              <div className="text-dark">{message}</div>
+              <small className="text-dark">{date}</small>
+            </div>
           </div>
-          <div className="col-lg-8 col-sm-8 col-8">
-            <strong className="text-primary">David John</strong>
-            <div>Lorem ipsum dolor sit amet, consectetur</div>
-            <small className="text-dark">27.11.2015, 15:00</small>
-          </div>
-        </div>
+        </Link>
       </li>
     );
   }
