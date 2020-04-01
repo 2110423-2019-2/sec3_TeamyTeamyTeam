bced66a3f7b419a3b70c9aedf047593c48954002
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-04-01 15:47:30 +0700

    Add uploader for photographer and comments

--- src/components/JobManagement/jobStatus.js
+++ src/components/JobManagement/jobStatus.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { storage } from "../../firebase";
 
 class JobStatus extends Component {
   constructor(props) {
@@ -10,23 +11,69 @@ class JobStatus extends Component {
       employer: "Nick",
       title: "Chill out at Chula",
       style: "Portrait",
-      //   date: new Date().toString().substr(4, 11),
-      date: "Jan 10 2019",
+      date: new Date().toString().substr(4, 11),
+      //   date: "Jan 10 2019",
       time: "Full day",
       location: "Chulalongkorn",
       totalFees: 299,
-      currency: "$"
+      currency: "$",
+      uploadProgress: 0,
+      isUploading: false,
+      file: null
     };
     this.getStatusMessageEmployer = this.getStatusMessageEmployer.bind(this);
     this.getStatusMessagePhotographer = this.getStatusMessagePhotographer.bind(
       this
     );
+    this.uploadImage = this.uploadImage.bind(this);
+    this.imgToBeUpload = this.imgToBeUpload.bind(this);
+  }
+
+  imgToBeUpload(e) {
+    let file = e.target.files[0];
+    if (file) this.setState({ file });
+  }
+
+  uploadImage() {
+    var uploadTask = storage
+      .ref()
+      .child(
+        this.state.employer +
+          "/images/" +
+          "jobID=" +
+          this.state.jobID +
+          "/" +
+          this.state.file.name
+      )
+      .put(this.state.file);
+    uploadTask.on(
+      "state_changed",
+      snapshot => {
+        const uploadProgress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        this.setState({ uploadProgress });
+        this.setState({ isUploading: true });
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+          this.setState({ isUploading: false });
+          this.setState({ statusCode: 7 });
+          //ใช้ตัวแปร downloadURL ได้เลยเพื่อส่งลิงก์
+          //ส่งdownload urlให้employer แล้วdrop job นี้ออกจากตาราง แล้วไปเพิ่มใน history
+        });
+      }
+    );
   }
 
   getStatusMessagePhotographer() {
+    //สำหรับphotographer
     const { statusCode } = this.state;
     switch (statusCode) {
-      case 2:
+      case 2: //เคสนี้ให้photographerรอ employer accept proposed fees ถ้าไม่ accept ก็drop job ทิ้ง
         return (
           <div>
             <h2>
@@ -43,7 +90,7 @@ class JobStatus extends Component {
             </h1>
           </div>
         );
-      case 3:
+      case 3: //แค่บอกให้รอemployerจ่ายเงิน
         return (
           <div>
             <h2>
@@ -61,7 +108,8 @@ class JobStatus extends Component {
             </h1>
           </div>
         );
-      case 4:
+      case 4: //เหมือนกับemployerเลย บอกให้รอถึงวันถ่ายกับถ้าถึงวันถ่ายจะมีปุ่มให้กดว่าเรียบร้อยแล้ว
+        // ถ้ากดปุ่มDoneแล้วก็จะเปลี่ยน statusCodeจาก 4 เป็น 5
         if (new Date().toString().substr(4, 11) !== this.state.date) {
           return (
             <div>
@@ -82,21 +130,23 @@ class JobStatus extends Component {
         } else {
           return (
             <div>
-              <h2>
-                <span class="badge badge-yellow">Appointed Day!</span>
-              </h2>
-              Meet <strong>{this.state.employer}</strong> at{" "}
-              <strong>{this.state.location}</strong>
               <h1>
                 <ion-icon
                   style={{ opacity: "0.5" }}
                   name="camera-outline"
                 ></ion-icon>
               </h1>
+              <h2>
+                <span class="badge badge-yellow">Appointed Day!</span>
+              </h2>
+              Meet <strong>{this.state.employer}</strong> at{" "}
+              <strong>{this.state.location}</strong>
+              <button className="btn btn-outline-light">Done shooting!</button>
+              {/* กดปุ่มเพื่อเปลี่ยนstatusCode */}
             </div>
           );
         }
-      case 5:
+      case 5: //บอกให้photographerรอemployerจ่ายเงินทั้งหมด
         return (
           <div>
             <h2>
@@ -113,7 +163,9 @@ class JobStatus extends Component {
             </h1>
           </div>
         );
-      case 6:
+      case 6: //เคสนี้ให้photographerอัปไฟล์zip
+        //พออัปเสร็จในfunction uploadImage จะมีcommentบอกอยู่ว่าให้ทำไร
+        //หลังเคสนี้จะเป็นstatusCode7ไม่ต้องไปอัปเดตในdatabaseแล้ว อัปเสร็จให้drop job ทิ้งเลยละไปเพิ่มลงส่วน history
         return (
           <div>
             <h2>
@@ -121,16 +173,39 @@ class JobStatus extends Component {
             </h2>
             <small>Upload your photos</small>
             <p>
-              <button className="btn btn-light">
-                <ion-icon
-                  name="cloud-upload"
-                  style={{ fontSize: "42px" }}
-                ></ion-icon>
-              </button>
+              {this.state.isUploading ? (
+                <div
+                  class="spinner-border"
+                  style={{ color: "#ffffff" }}
+                  role="status"
+                ></div>
+              ) : (
+                <button
+                  className="btn btn-light"
+                  onClick={() => document.getElementById("uploader").click()}
+                >
+                  <ion-icon
+                    name="cloud-upload"
+                    style={{ fontSize: "42px" }}
+                  ></ion-icon>
+                </button>
+              )}
             </p>
+            <button
+              onClick={this.uploadImage}
+              className="btn btn-outline-light"
+            >
+              Submit file
+            </button>
+            <input
+              type="file"
+              id="uploader"
+              style={{ display: "none" }}
+              onChange={this.imgToBeUpload}
+            />
           </div>
         );
-      case 7:
+      case 7: //อันนี้back-endไม่ต้องทำไร ส่วนแสดงผลเฉย ๆ
         return (
           <div>
             <h2>
@@ -142,9 +217,11 @@ class JobStatus extends Component {
   }
 
   getStatusMessageEmployer() {
+    //สำหรับemployer
     const { statusCode } = this.state;
     switch (statusCode) {
-      case 1:
+      case 1: //เคสนี้คือส่งofferไปแล้วรอphotographerตอบ
+        //ถ้าacceptระบบก็ต้องเปลี่ยนstatusCodeให้เป็น2 คือไปอัปเดตในฐานข้อมูล แต่ถ้าdeclineก็drop jobนี้ทิ้งไปเลย
         return (
           <div>
             <h2>
@@ -159,7 +236,8 @@ class JobStatus extends Component {
             </h1>
           </div>
         );
-      case 2:
+      case 2: //อันนี้เป็นฝั่งเราตอบรับราคาที่photographerเสนอมา ถ้ารับก็เปลี่ยนstatusCodeเป็น3
+        //ถ้าไม่รับก็drop job นี้ทิ้ง
         return (
           <div>
             <h1>
@@ -175,10 +253,12 @@ class JobStatus extends Component {
             <button className="mr-3 btn btn-sm btn-outline-light">
               Accept
             </button>
+            {/* เขียนฟังก์ชันขึ้นมาเพื่อเปลี่ยนstatusCodeในdatabase */}
             <button className="btn btn-sm btn-outline-warning">Decline</button>
           </div>
         );
-      case 3:
+      case 3: //เคสนี้ก็ให้เปลี่ยนstatusCodeก็ต่อเมื่อจ่าย30%แล้ว
+        //ถ้าถึงวันถ่ายแล้วไม่จ่ายให้drop job นี้ทิ้ง
         return (
           <div>
             <h1>
@@ -204,7 +284,7 @@ class JobStatus extends Component {
             </button>
           </div>
         );
-      case 4:
+      case 4: //อันนี้ไม่มีอะไรแค่แสดงผลว่าให้รอถึงวันถ่ายภาพ กับบอกว่าวันนี้เป็นวันถ่ายภาพ
         if (new Date().toString().substr(4, 11) !== this.state.date) {
           return (
             <div>
@@ -239,7 +319,7 @@ class JobStatus extends Component {
             </div>
           );
         }
-      case 5:
+      case 5: //เคสนี้ก็ให้employerจ่ายเงินที่เหลือ จ่ายสำเร็จก็เปลี่ยนstatusCodeเป็น6
         return (
           <div>
             <h1>
@@ -264,7 +344,7 @@ class JobStatus extends Component {
             </button>
           </div>
         );
-      case 6:
+      case 6: //แค่บอกให้employerรอphotographerส่งภาพมา
         return (
           <div>
             <h2>
@@ -279,23 +359,24 @@ class JobStatus extends Component {
             </h1>
           </div>
         );
-      case 7:
-        return (
-          <div>
-            <h2>
-              <span class="badge badge-info">Here's your photo!</span>
-            </h2>
-            <small>Download them</small>
-            <p>
-              <button className="btn btn-light">
-                <ion-icon
-                  name="download"
-                  style={{ fontSize: "42px" }}
-                ></ion-icon>
-              </button>
-            </p>
-          </div>
-        );
+      //   case 7:
+      // //เคส7เขียนเผื่อไว้ แต่ไม่น่าจะใช้ จะให้ photographer ส่งลิงก์ไฟล์มาให้ โดยจะไปปรากฎที่ history แทนแล้ว ไม่ใช่ job
+      //     return (
+      //       <div>
+      //         <h2>
+      //           <span class="badge badge-info">Here's your photo!</span>
+      //         </h2>
+      //         <small>Download them</small>
+      //         <p>
+      //           <button className="btn btn-light">
+      //             <ion-icon
+      //               name="download"
+      //               style={{ fontSize: "42px" }}
+      //             ></ion-icon>
+      //           </button>
+      //         </p>
+      //       </div>
+      //     );
     }
   }
 
