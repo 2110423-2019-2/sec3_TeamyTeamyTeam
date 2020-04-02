964973d3d071bed6a20b453ca119944418f5721f
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-04-02 08:53:30 +0700

    Make review modal auto show

--- src/components/Payment/payment.js
+++ src/components/Payment/payment.js
@@ -13,26 +13,20 @@ export class CheckoutPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< HEAD
       cart: {
         email: "guest@test.com",
         name: "Guest",
         //items: [],
-        amount: 100000,
+        amount: 100000
         //totalQty: 0
       },
-=======
-      email: "guest@test.com",
-      name: "test",
-      amount: 10000,
->>>>>>> 74acb61c6ee1b02f34194f0983fbce94022cd5c1
       charge: undefined
     };
   }
 
-  change = () =>{
-    console.log(this.state.cart)
-  }
+  change = () => {
+    console.log(this.state.cart);
+  };
   createCreditCardCharge = async (email, name, amount, token) => {
     try {
       const res = await axios({
@@ -74,14 +68,12 @@ export class CheckoutPage extends Component {
   };
 
   render() {
-<<<<<<< HEAD
-    const { cart,charge } = this.state;
+    const { cart, charge } = this.state;
     return (
       <div className="own-form">
         <div className="cart__summary">
           <h2>Cart Summary</h2>
-          <div className="cart-details">
-          </div>
+          <div className="cart-details"></div>
         </div>
         <ChekoutCreditCard
           cart={cart}
@@ -111,48 +103,8 @@ export class CheckoutPage extends Component {
             </div>
           )}
         </div>
-      
-        <button onClick = {this.change}></button>
-=======
-    const charge = this.charge;
-    return (
-      <div className="container" style={{ marginTop: "-54px" }}>
-        <div className="row h-100 align-items-center">
-          <div className="col-12 text-center ">
-            <h1 style={{ margin: "5vh" }}>Please select your payment method</h1>
-            <ChekoutCreditCard
-              createCreditCardCharge={this.createCreditCardCharge}
-            />
-            <div>
-              <CheckoutInternetBanking
-                createInternetBankingCharge={this.createInternetBankingCharge}
-              />
-            </div>
-            <div className="message">
-              {charge && (
-                <div>
-                  <h4>Thank you for your payment with credit card.</h4>
-                  <p>
-                    Your payment amount is{" "}
-                    <span
-                      className={
-                        charge.status === "successful"
-                          ? "success"
-                          : charge.status === "failed"
-                          ? "failed"
-                          : "pending"
-                      }
-                    >
-                      {charge.status}
-                    </span>
-                  </p>
-                  e
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
->>>>>>> 74acb61c6ee1b02f34194f0983fbce94022cd5c1
+
+        <button onClick={this.change}></button>
       </div>
     );
   }
